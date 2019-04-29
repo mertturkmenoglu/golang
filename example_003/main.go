40369bfc34468ce890176b93b77b79ce9c5ca765
Mert <mertturkmenoglu99@gmail.com>
2019-04-30 02:04:21 +0300

    Add variable example Initializing and declaring variables, language syntax and naming conventions

---
+++ example_003/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/**
+ * Variables example
+ */
+func main() {
+	// Declaring and initializing a variable
+	var str1 = "Hello"
+	fmt.Println(str1)
+
+	// Declaring multiple variables
+	var int1, int2 int = 1, 2
+	fmt.Println(int1, int2)
+
+	// Declaring Boolean
+	var isTrue = true
+	fmt.Println(isTrue)
+
+	// Not initializing. Default is 0 for integer
+	var int3 int
+	fmt.Println(int3)
+
+	// Declaring and initializing: Shorthand syntax
+	str2 := "World"
+	fmt.Println(str2)
+}
