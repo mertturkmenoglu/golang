12507ce182b7d90ec102b19cd5b58c7d4e5b2b59
Mert <mertturkmenoglu99@gmail.com>
2019-05-10 22:59:52 +0300

    Add multiple return values example

---
+++ example_015/main.go
@@ -0,0 +1,22 @@
+/**
+ * Multiple return values example
+ */
+
+package main
+
+import "fmt"
+
+func evenOdd(number int) (string, int) {
+	if number%2 == 0 {
+		return "even", 0
+	} else {
+		return "odd", 1
+	}
+}
+
+func main() {
+	twenty := 20
+	resultStr, resultInt := evenOdd(twenty)
+	fmt.Println("String:", resultStr)
+	fmt.Println("Integer:", resultInt)
+}
