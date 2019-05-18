7192dca77441fd754802a33c7595b47cda078216
Mert <mertturkmenoglu99@gmail.com>
2019-05-18 22:31:57 +0300

    Add methods example

---
+++ example_020/main.go
@@ -0,0 +1,36 @@
+/**
+ * Example 20: Methods
+ *
+ * Methods examples.
+ */
+package main
+
+import "fmt"
+import "math"
+
+// Struct definition
+type circle struct {
+	radius float32
+}
+
+// Reciever type: circle*
+func (c *circle) area() float32 {
+	return c.radius * c.radius * math.Pi
+}
+
+// Reciever type: value
+func (c circle) perimeter() float32 {
+	return 2 * math.Pi * c.radius
+}
+
+// Driver code to test methods
+func main() {
+	c := circle{radius: 1}
+
+	fmt.Println("Circle area: ", c.area())
+	fmt.Println("Circle perimeter:", c.perimeter())
+
+	pointerC := &c
+	fmt.Println("Circle area: ", pointerC.area())
+	fmt.Println("Circle perimeter:", pointerC.perimeter())
+}
