922bbe344691dc4da272af1b49538310db4ed3e0
Mert Türkmenoğlu <34170799+mertturkmenoglu@users.noreply.github.com>
2019-06-04 02:31:59 +0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+/*
+ * Go examples: Find minimum integer in an array
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+    var array [10]int
+    
+	for i := 0; i < 10; i++ {
+        fmt.Println("Element: ")
+        fmt.Scan(&array[i])
+	}
+    
+    minIndex := 0
+    
+    for i := 1; i < 10; i++ {
+        if (array[i] < array[minIndex]) {
+            minIndex = i
+        }
+    }
+    
+    fmt.Println(array[minIndex])
+}
