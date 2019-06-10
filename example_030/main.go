8833d0e3acd38a5f6b819a6127de0669601c77e9
Mert <mertturkmenoglu99@gmail.com>
2019-06-10 17:57:02 +0300

    Add goroutines example

---
+++ example_030/main.go
@@ -0,0 +1,32 @@
+/**
+ * Goroutines example
+ */
+
+package main
+
+import "fmt"
+
+// We will try to run this function in a thread
+// It takes a string and prints it for an arbitrary number of times
+func printMessage(message string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(message, ":", i)
+	}
+}
+
+// Driver code
+func main() {
+	// A normal function call
+	printMessage("Calling directly. Normal call")
+
+	// Invoking in a thread
+	go printMessage("Calling in a goroutine")
+
+	// Anonymous function call in a goroutine
+	go func(message string) {
+		fmt.Println(message)
+	}("go running")
+
+	fmt.Scanln()
+	fmt.Println("Program ends")
+}
