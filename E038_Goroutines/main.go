3aa7dc49221a31ff3e9676e9d5eba27ae3286b2d
Mert <mertturkmenoglu99@gmail.com>
2019-06-25 14:25:38 +0300

    Add goroutine example

---
+++ E038_Goroutines/main.go
@@ -0,0 +1,22 @@
+/**
+ * Goroutines example
+ */
+
+package main
+
+import "fmt"
+
+func printMessage(message string, times int) {
+	for i := 0; i < times; i++ {
+		fmt.Println(message, "-", i)
+	}
+}
+
+func main() {
+	go printMessage("Goroutine", 100)
+	go printMessage("Emily", 100)
+	go printMessage("Diana", 100)
+	go printMessage("Babushka", 100)
+
+	fmt.Scanln()
+}
